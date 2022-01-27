303f3fb8d19709d08abefafe264c77f6944a11a3
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-01-27 15:25:16 -0500

    Add code from first CodePen

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>Breakfast is grrrrreat!</h1>
+      <p>Today I had cereal.</p>
+
+      <h2>My favorite cereal</h2>
+      <p>is Frosted Flakes.</p>
+
+      <h3>Don't be mad but...</h3>
+      <p>I put milk in before my cereal</p>
+
+      <button> yummy </button>
+      <a href="https://www.frostedflakes.com/en_US/home.html"> Click me to learn more </a>
+      <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d28ec53d-aed5-4899-b69f-e8e458078bca/d62wcwf-3d043dbb-922a-4a70-afff-4e159c708227.png/v1/fill/w_382,h_832,strp/tony_the_tiger_by_mollyketty_d62wcwf-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04MzIiLCJwYXRoIjoiXC9mXC9kMjhlYzUzZC1hZWQ1LTQ4OTktYjY5Zi1lOGU0NTgwNzhiY2FcL2Q2Mndjd2YtM2QwNDNkYmItOTIyYS00YTcwLWFmZmYtNGUxNTljNzA4MjI3LnBuZyIsIndpZHRoIjoiPD0zODIifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.hmUMbAP0ySjNueaX1v5BciF-M3DhSXuZeAOPZ5ByflA" alt="Tony the Tiger"/>
+    </main>
   );
 }
 
